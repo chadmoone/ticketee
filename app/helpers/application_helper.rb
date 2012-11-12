ea1d1133bb030ac61f1721c5673600b6000132e6
Chad Moone <chadmoone@gmail.com>
2012-11-12 18:32:35 -0500

    fix errant concat, which caused layout to render twice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
 	def title(*parts)
 		unless parts.empty?
 			content_for :title  do
-				(parts << "Ticketee").join(" - ")
+				(parts << "Ticketee").join(" - ") unless parts.empty?
 			end
 		end
 	end
 
   def admins_only(&block)
-    concat(block.call) if current_user.try(:admin?)
+    block.call if current_user.try(:admin?)
   end
 
   def authorized?(permission, thing, &block)

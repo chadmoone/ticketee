b42547cd4ebff634c80b3affdab193d2bbda53fd
Chad Moone <chadmoone@gmail.com>
2013-01-16 12:59:13 -0500

    Fix for Admin not having some permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,13 +2,18 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user.permissions.each do |permission|
-      can permission.action.to_sym,
-          permission.thing_type.constantize do |thing|
-            thing.nil? ||
-            permission.thing_id.nil? ||
-            permission.thing_id == thing.id
-          end
+    user ||= User.new # guest user (not logged in)
+    
+    if user.admin?
+      can :manage, :all
+    else
+      user.permissions.each do |permission|
+        can permission.action.to_sym, permission.thing_type.constantize do |thing|
+          thing.nil? ||
+          permission.thing_id.nil? ||
+          permission.thing_id == thing.id
+        end
+      end
     end
   end
   

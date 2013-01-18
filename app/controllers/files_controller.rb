9fe84ee7c3f6186c9588ad3c2b1d83c66e9b3e22
Chad Moone <chadmoone@gmail.com>
2013-01-18 17:13:07 -0500

    Provide an 'Add another file' link that uses javascript so that users can upload more than one file

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -8,7 +8,12 @@ class FilesController < ApplicationController
     else
       flash[:alert] = "The asset you were looking for could not be found."
       redirect_to root_path
-    end
-    
+    end    
+  end
+
+  def new
+    @ticket = Ticket.new
+    @asset = @ticket.assets.build
+    render partial: "files/form", locals: {number: params[:number].to_i, asset: @asset}
   end
 end
